c630c8a960d46af23a8ee80a9a42b59f990f15a6
Nidhi Choudhary <nidhichoudhary@Nidhis-MacBook-Air.local>
2022-01-05 14:34:38 +0530

    Step 24 completed

---
+++ src/main/java/com/nichoudhary/rest/webservices/restfulwebservices/filtering/filteringController.java
@@ -0,0 +1,22 @@
+package com.nichoudhary.rest.webservices.restfulwebservices.filtering;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class filteringController {
+
+    @GetMapping("/filtering")
+    public SomeBean retrieveSomeBean() {
+        return new SomeBean("field1", "field2", "field3");
+    }
+
+    @GetMapping("/filtering-list")
+    public List<SomeBean> retrieveAllSomeBean() {
+        return Arrays.asList(new SomeBean("field1", "field2", "field3"), new SomeBean("value1", "value2", "value3"));
+    }
+}
