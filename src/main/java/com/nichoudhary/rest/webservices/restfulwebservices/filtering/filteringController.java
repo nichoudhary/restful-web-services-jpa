5901b5b3384a1283c35ebe0da1ebdbe58d16f6f5
Nidhi Choudhary <nidhichoudhary@Nidhis-MacBook-Air.local>
2022-01-05 14:52:54 +0530

    Step 25 completed

--- src/main/java/com/nichoudhary/rest/webservices/restfulwebservices/filtering/filteringController.java
+++ src/main/java/com/nichoudhary/rest/webservices/restfulwebservices/filtering/filteringController.java
@@ -1,6 +1,10 @@
 package com.nichoudhary.rest.webservices.restfulwebservices.filtering;
 
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +14,27 @@ import java.util.List;
 @RestController
 public class filteringController {
 
+    // In this request, we only want to send field1 and field2
     @GetMapping("/filtering")
-    public SomeBean retrieveSomeBean() {
-        return new SomeBean("field1", "field2", "field3");
+    public MappingJacksonValue retrieveSomeBean() {
+
+        SomeBean someBean = new SomeBean("field1", "field2", "field3");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
+    // In this request, we only want to send field2 and field3
     @GetMapping("/filtering-list")
-    public List<SomeBean> retrieveAllSomeBean() {
-        return Arrays.asList(new SomeBean("field1", "field2", "field3"), new SomeBean("value1", "value2", "value3"));
+    public MappingJacksonValue retrieveAllSomeBean() {
+        List<SomeBean> list =  Arrays.asList(new SomeBean("field1", "field2", "field3"), new SomeBean("value1", "value2", "value3"));
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(list);
+        mapping.setFilters(filters);
+        return mapping;
     }
 }
